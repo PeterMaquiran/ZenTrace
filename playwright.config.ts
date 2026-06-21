5a7ea1272fdbd4f18918005fd042f5b747220acb
Peter Maquiran <maquiranpeter29@gmail.com>
2026-06-21 23:56:26 +0100

    test(github-workflow): add xvfb-run to github workflow to playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:5173',
 
-    headless: false, // required for Chrome extension loading
+    headless: process.env.CI ? true : false, // required for Chrome extension loading
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
